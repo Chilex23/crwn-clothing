b13a05acb794f318d25d3632d10ecb0958e54a79
Chilex23 <onumaegbuchilex@gmail.com>
2022-05-30 22:30:12 +0100

    Update our men items to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop/hats' element={HatsPage()} />
+      </Routes>
+      {/* <HomePage /> */}
     </div>
   );
 }
