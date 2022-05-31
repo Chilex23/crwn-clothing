34a4991d416c7a79dcd19be83bbe3eef94a3258a
Chilex23 <onumaegbuchilex@gmail.com>
2022-05-31 12:37:59 +0100

    Added Header components and link

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
       </Routes>
-      {/* <HomePage /> */}
     </div>
   );
 }
