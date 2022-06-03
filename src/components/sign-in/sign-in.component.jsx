4584b690fb36a91deb42769ec3924d5a958730fa
Chilex23 <onumaegbuchilex@gmail.com>
2022-06-03 19:07:25 +0100

    Integrated redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -38,7 +38,7 @@ class SignIn extends React.Component {
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
                     <div className="buttons">
                         <CustomButton type="submit"> Sign In </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignedIn> Sign In With Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignedIn> Sign In With Google</CustomButton>
                     </div>
                 </form>
             </div>
