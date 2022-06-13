3dc68f388f9e802ae69baa2c37af4820d6001b7d
Chilex23 <onumaegbuchilex@gmail.com>
2022-06-13 22:09:04 +0100

    removing redux-logger from production build

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -50,7 +50,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    return docSnap.data();
+    console.log("Document data:", docSnap.data());
   } else {
     // doc.data() will be undefined in this case
     const { displayName, email } = userAuth;
